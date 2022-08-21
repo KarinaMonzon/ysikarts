822f171693d838b35ef57eae70865633abb4be9b
KarinaMonzon <karinamon1849@gmail.com>
2022-08-21 19:42:45 -0300

    ¨cambios¨

--- js/app.js
+++ js/app.js
@@ -6,3 +6,24 @@ function abrir(){
         behavior: 'smooth'
     })
 }
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_by48zaa';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      window.location.href ='index.html';
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
